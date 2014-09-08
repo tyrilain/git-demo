166a4bd971361b4f098f724789387d85316b3b0e
= <=>
2014-09-08 13:59:25 -0700

    Less enthusiasm

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def hello():
-    # prints "Hello World!!!"
-    print "Hello World!!!"
+    # prints "Hello World"
+    print "Hello World"
 
-if __name__=="__main__"
+if __name__=="__main__":
     print hello()
     print hello()
